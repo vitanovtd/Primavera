61b0c76ea067853b09a3d835534414a32f617513
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-03-03 22:33:13 +0200

    updated navbar component

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -6,7 +6,27 @@ import "./Navbar.scss";
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="wrapper">Navbar</div>
+            <div className="wrapper">
+                <div className="left">
+                    <div className="item">
+                        <Link className="logo" to="/">Primavera</Link>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="item">
+                        <Link className="link" to="/">Home</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">Menu</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">Reservation</Link>
+                    </div>
+                    <div className="item">
+                        <Link className="link" to="/">Our Location</Link>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
