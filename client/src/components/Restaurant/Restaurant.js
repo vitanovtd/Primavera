7263dc9f20f5fa55d23ce94111fdced6ba0c99a1
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-03-25 00:40:14 +0200

    Updated styling for restaurant component

--- client/src/components/Restaurant/Restaurant.js
+++ client/src/components/Restaurant/Restaurant.js
@@ -4,15 +4,20 @@ import "./Restaurant.scss"
 const Restaurant = () => {
     return (
         <div className="restaurant">
-            <div className="container">
+            <div className="resume">
                 <div className="left">
                     <div className="heading">
                         <h1>Discover</h1>
                         <h3>Our Restaurant</h3>
                         <span>*****</span>
                     </div>
-                    <p></p>
-                    <Link>Contact us</Link>
+                    <p> "At vero eos et accusamus et iusto odio
+                        dignissimos ducimus qui blanditiis praesentium
+                        voluptatum deleniti atque corrupti quos dolores et quas
+                        molestias excepturi sint occaecati cupiditate non provident,
+                        similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
+                        Et harum quidem rerum facilis est et expedita distinctio.</p>
+                    <Link className="link" to={"/contact"}>Contact us</Link>
                 </div>
                 <div className="right">
                     <div className="imageRes">
