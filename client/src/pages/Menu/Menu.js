2e7fb47b490fdaa4f5554686406f4349dbd3564d
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-03-26 23:36:18 +0300

    added menu base

--- client/src/pages/Menu/Menu.js
+++ client/src/pages/Menu/Menu.js
@@ -4,7 +4,33 @@ import "./Menu.scss";
 
 const Menu = () => {
     return (
-        <div className="menu"></div>
+        <div className="menu">
+            <div className="left">
+                <div className="filterItem">
+                    <h2>Meal Category</h2>
+                    <div className="inputItem">
+                        <input type="checkbox" id="1" value={1} />
+                        <label htmlFor="1">Salads</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="2" value={2} />
+                        <label htmlFor="2">Starters</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="3" value={3} />
+                        <label htmlFor="3">Main dishes</label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="4" value={4} />
+                        <label htmlFor="4">Deserts</label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="right">
+
+            </div>
+        </div>
     )
 }
 
