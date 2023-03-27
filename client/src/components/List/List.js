e8eb3a3e065350ebf75b853c4ea31e29b11cb207
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-03-27 22:39:55 +0300

    added List holder for cardMeals

---
+++ client/src/components/List/List.js
@@ -0,0 +1,13 @@
+import './List.scss';
+import CardMeal from '../CardMeal/CardMeal';
+
+const List = () => {
+    const data = [];
+    return (
+        <div className='list'>
+            Hello there
+        </div>
+    )
+}
+
+export default List;
