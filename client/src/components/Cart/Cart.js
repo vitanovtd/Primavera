f48b4515ed88f77a60dc7dce70402fe86d9d164e
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-03-28 23:39:46 +0300

    modified Cart component

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,9 +1,35 @@
 import "./Cart.scss";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+
 
 const Cart = () => {
+
+
+    const data = [];
+
     return (
         <div className="cart">
+            <h1>Items in your cart</h1>
+            {data?.map(item => (
+                <div className="item" key={item.id}>
+                    <img src={item.img} alt="" />
+                    <div className='details'>
+                        <h1>{item.title}</h1>
+                        <p>{item.desc.substring(0, 100)}</p>
+                        <div className='price'> 1 x ${item.price}</div>
+                    </div>
+                    <DeleteOutlinedIcon
+                        className='delete'
 
+                    />
+                </div>
+            ))}
+            <div className='total'>
+                <span>SUBTOTAL</span>
+                <span>$123</span>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+            <span className='reset'>Reset Cart</span>
         </div>
     )
 }
