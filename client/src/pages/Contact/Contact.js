a2150400c853c5391f7d8179918cb1b1be764e7e
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-03-29 23:07:07 +0300

    added Google Maps API and component holders for Contact page

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,9 +1,29 @@
 import './Contact.scss';
+import Map from './Map';
+
+
+const location = {
+    address: 'Sofia city center 1000',
+    lat: 42.69751,
+    lng: 23.32415,
+}
 
 const Contact = () => {
+
     return (
         <div className="about">
 
+            <div className="introContainer">
+                <h2 className="introTitte">Our Mission</h2>
+
+                <p className="introText">
+                    Every single person who understands the impact of the web should be an
+                    advocate and strive for a better, more inclusive web for all.
+                </p>
+            </div>
+
+
+            <Map location={location} zoomLevel={15} />
         </div>
     )
 }
---
+++ client/src/pages/Contact/Map.js
@@ -0,0 +1,42 @@
+import GoogleMapReact from 'google-map-react'
+import { GoogleMap, useLoadScript, MarkerF } from '@react-google-maps/api'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import './Map.scss';
+
+
+const LocationPin = ({ text }) => (
+    <div className='pin'>
+        <LocationOnIcon className='pinIcon' />
+        <p className='pinText'>{text}</p>
+    </div>
+)
+
+
+
+
+
+const Map = ({ location, zoomLevel }) =>
+(
+    <div className="map">
+        <h2 className="mapText">Come Visit Our Restaurant</h2>
+        <div className="googleMap">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
+                mapContainerClassName="map-container"
+                defaultCenter={location}
+                defaultZoom={zoomLevel}
+            >
+                <LocationPin
+                    lat={location.lat}
+                    lng={location.lng}
+                    text={location.address}
+                />
+
+            </GoogleMapReact>
+        </div>
+    </div >
+)
+
+
+
+export default Map;
