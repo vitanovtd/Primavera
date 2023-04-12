5b4497dfcf56548e5c737c48b2bfb19e2de3f1ff
Tihomir Vitanov <96017431+vitanovtd@users.noreply.github.com>
2023-04-12 10:47:06 +0300

    added redux persistor for Cart state

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
-
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
